c4f0eb0b8f8bc30ef755d17a0c5b939d6ed11cff
eloisemaire24 <eloisemaire24@gmail.com>
2021-04-24 10:40:02 +0200

    maj questionnaire

--- app/home/content_gen/questionaire.py
+++ app/home/content_gen/questionaire.py
@@ -8,7 +8,16 @@ class QuestionairesAgri(FlaskForm):
 	name_exploit = wtforms.TextField('Nom exploitation', validators=[DataRequired()])
 	address   = wtforms.TextField('Address', validators=[DataRequired()])
 	age = wtforms.TextField('Age', validators=[DataRequired()])
-	date = wtforms.DateField('Date')
+	sau = wtforms.TextField('sau', validators=[DataRequired()])
+	etp = wtforms.TextField('etp', validators=[DataRequired()])
+	haie = wtforms.SelectField(u'Presence haies', choices=[('y1', 'oui sur toutes les parcelles'), ('y2', 'oui sur une partie des parcelles'), ('no', 'non')])
+	cepage = wtforms.SelectMultipleField(choices=[('cep1', 'cabernet sauvignon'), ('cep2', 'carignan'), ('cep3', 'grenache noir'), ('cep4', 'syrah'), ('cep5', 'muscat'), ('cep6', 'chardonnay'), ('cep7', 'cinsault')
+	autract = wtforms.TextField('autre activite', validators=[DataRequired()])
+	autrcult = wtforms.SelectField(u'autre cultures', choices=[('y', 'oui'), ('n', 'non')])
+	typecult = wtforms.TextField('type culture', validators=[DataRequired()])
+	typefonc = wtforms.SelectField(u'type de foncier', choices=[('prop', 'proprietaire'), ('loc', 'locataire'), ('mist', 'proprietaire et locataire')])
+	certif = 
+
 	language = wtforms.SelectField(u'Programming Language', choices=[('cpp', 'C++'), ('py', 'Python'), ('text', 'Plain Text')])
 	text = wtforms.TextAreaField('Text multi ligne')
 	selection = wtforms.RadioField(choices=[('ch1', 'Choix 1'), ('ch1', 'Choix 2'), ('ch1', 'Choix 3')]) #1ere valeur du tuple: ce qui s'affichera dans la DB et 2eme: ce qui s'affiche dans l'HTML
